e87243bcaa56b8f7925dc1924af146784fec2ff9
nat730 <natanaelcolart@gmail.com>
2023-10-17 17:05:25 +0200

    jpeteuncable

--- src/card.ts
+++ src/card.ts
@@ -1,62 +1,117 @@
-class Card {
-  private static cardCount = 0;
-  private rank: string;
-  private suit: string;
-  public id?: string;
+const player = document.createElement("div");
+player.classList.add("player1");
+document.getElementById("GameBoard")!.appendChild(player);
 
-  constructor(rank: string, suit: string) {
+class Card {
+  public static cardCount = 0;
+  public rank: string;
+  public suit: string;
+  public id?: string;
+  public isVerso: boolean;
+
+  constructor(rank: string, suit: string, isVerso: boolean = false) {
     this.rank = rank;
     this.suit = suit;
+    this.isVerso = isVerso;
   }
 
   createCard() {
     const cardDiv = document.createElement("div");
     cardDiv.className = "card";
-    
-    cardDiv.id = `card${Card.cardCount}`;
-    Card.cardCount++;
 
-    if(this.id){
-      cardDiv.id = `${this.id}`
+    if (this.id === undefined) {
+      this.id = `card${Card.cardCount}`;
+      Card.cardCount++;
+    }
+
+    if (this.id) {
+      cardDiv.id = `${this.id}`;
     }
 
     const cardDonneesDiv = document.createElement("div");
     cardDonneesDiv.className = "card_Donnees";
 
-    const topLeftDiv = document.createElement("div");
-    topLeftDiv.className = "card_Donnees_top_left";
-    topLeftDiv.innerHTML = `${this.rank} <br> &${this.suit};`;
+    if (this.isVerso) {
+      cardDonneesDiv.innerHTML = "Verso";
+    } else {
+      const topLeftDiv = document.createElement("div");
+      topLeftDiv.className = "card_Donnees_top_left";
 
-    const centerDiv = document.createElement("div");
-    centerDiv.className = "card_Donnees_center";
-    centerDiv.innerHTML = `&${this.suit};`;
+      if (this.suit === "hearts" || this.suit === "diams") {
+        topLeftDiv.style.color = "red";
+      }
 
-    const botRightDiv = document.createElement("div");
-    botRightDiv.className = "card_Donnees_bot_right";
-    botRightDiv.innerHTML = `${this.rank} <br> &${this.suit};`;
+      topLeftDiv.innerHTML = `${this.rank} <br> &${this.suit};`;
 
-    cardDonneesDiv.appendChild(topLeftDiv);
-    cardDonneesDiv.appendChild(centerDiv);
-    cardDonneesDiv.appendChild(botRightDiv);
+      const centerDiv = document.createElement("div");
+      centerDiv.className = "card_Donnees_center";
+      if (this.suit === "hearts" || this.suit === "diams") {
+        centerDiv.style.color = "red";
+      }
+
+      centerDiv.innerHTML = `&${this.suit};`;
+
+      const botRightDiv = document.createElement("div");
+      botRightDiv.className = "card_Donnees_bot_right";
+
+      if (this.suit === "hearts" || this.suit === "diams") {
+        botRightDiv.style.color = "red";
+      }
+
+      botRightDiv.innerHTML = `${this.rank} <br> &${this.suit};`;
+
+      cardDonneesDiv.appendChild(topLeftDiv);
+      cardDonneesDiv.appendChild(centerDiv);
+      cardDonneesDiv.appendChild(botRightDiv);
+    }
 
     cardDiv.appendChild(cardDonneesDiv);
 
-    document.getElementById("GameBoard")!.appendChild(cardDiv);
+    player.appendChild(cardDiv);
   }
 }
 
-  const suits: string[] = ["spades"];
-  const ranks: string[] = ["K", "Q", "J", "A", "C", "10"];
+const suits: string[] = ["spades", "hearts", "clubs", "diams"];
+const ranks: string[] = ["K", "Q", "J", "A", "10", "9", "8", "7", "6", "5", "4", "3", "2"];
 
-  for (let i = 0; i < suits.length; i++) {
-    for (let j = 0; j < ranks.length; j++) {
-      const card = new Card(ranks[j], suits[i]);
-      card.createCard();
-    }
+const allCards: { rank: string; suit: string }[] = [];
+for (const suit of suits) {
+  for (const rank of ranks) {
+    allCards.push({ rank, suit });
+    
   }
+}
 
-  for (let k = 1; k <= 3; k++) {
-    const defausseCard = new Card("10", "spades");
-    defausseCard.id = `defausse${k}`;
-    defausseCard.createCard();
-  };
+const shuffledCards = shuffleArray(allCards);
+console.log("shuffledCards", shuffledCards);
+
+for (let i = 0; i < 4; i++) {
+  for (let j = 0; j < 5; j++) {
+    const card = new Card(shuffledCards.pop()!.rank, shuffledCards.pop()!.suit);
+    card.createCard();
+  }
+}
+
+for (let k = 1; k <= 4; k++) {
+  const defausseCard = new Card(shuffledCards.pop()!.rank, shuffledCards.pop()!.suit);
+  defausseCard.id = `defausse${k}`;
+  defausseCard.createCard();
+}
+
+for (let l = 1; l <= Card.cardCount - 24; l++) {
+  const sidePileCard = new Card(shuffledCards.pop()!.rank, shuffledCards.pop()!.suit); // Adjust as needed
+  sidePileCard.createCard();
+}
+
+function shuffleArray(array: any[]) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  console.log("jkhkj,", array);
+  
+  return array;
+}
+
+
+
